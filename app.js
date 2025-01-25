d1243767cbf111d28831e16891ae86850d1269c2
DemissonAndrade <demissondsa@gmail.com>
2025-01-24 21:18:06 -0300

    connect DB

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-require ('dotenv').config()
+require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
-const bcrypt = require ('bcrypt');
-const jwt =require ('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.status(200).json({ msg: "bem vindo minha API"})
+//Open routs
+app.get('/', (req, res) => {
+    res.status(200).json({ msg: "Meu projeto" })
 })
 
-app.listen(3000)
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+mongoose
+    .connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.x9vg9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+    .then(() => {
+        app.listen(5001)
+        console.log('Conectou ao Banco!')
+    })
+    .catch((err) => console.log(err))
+
+app.listen(5000);
